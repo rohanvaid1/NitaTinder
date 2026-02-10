7138c15568ab74eb0226d30a46974db5a780f360
= <=>
2026-02-10 20:28:22 +0530

    Add tailwind and daisy ui

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
  - Inilize git
  - Remove extra file and code and create hello world
  - Create .env file and add node_modules
+
+ - Add tailwind css and daisy UI
--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </>
   )
 }
